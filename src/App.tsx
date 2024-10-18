eebc1a4516f22d23cdd707d5a610e6fdecf27bbc
loopy-lim <loopy@jnu.ac.kr>
2024-10-18 13:05:18 +0900

    feat: add react-qeury setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<div>
-			<h1>Counter</h1>
-			<p>{count}</p>
-			<button onClick={() => setCount(count + 1)}>Increment</button>
-			<button onClick={() => setCount(count - 1)}>Decrement</button>
-		</div>
-	);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
