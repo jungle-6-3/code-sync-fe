cc8e8b4295b7230a2ae6d51e151c264dcf86343c
loopy-lim <loopy@jnu.ac.kr>
2024-10-21 13:41:27 +0900

    feat: add tailwind prettier

--- src/pages/ConversationPage.tsx
+++ src/pages/ConversationPage.tsx
@@ -1,12 +1,18 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
 const ConversationPage = () => {
-  const { conversationId } = useParams<{ conversationId: string }>();
+  // const { conversationId } = useParams<{ conversationId: string }>();
 
   return (
-    <div>
-      <h1>Conversation</h1>
-      <p>Conversation ID: {conversationId}</p>
+    <div className="flex h-screen flex-col">
+      <div className="flex max-h-screen flex-1">
+        <nav className="w-20 bg-slate-400">nav</nav>
+        <div className="w-64 bg-yellow-300">left</div>
+        <div className="flex flex-1 flex-col">
+          <div className="flex-1">body</div>
+          <div className="h-52 bg-lime-300">bottom</div>
+        </div>
+      </div>
     </div>
   );
 };
