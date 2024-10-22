6581c35c2363f3aa5b19fcd3bb8041b8a1a781eb
loopy-lim <loopy@jnu.ac.kr>
2024-10-22 20:16:50 +0900

    fix: add credentials for cookie

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -3,5 +3,6 @@ import { io } from 'socket.io-client';
 const URL = import.meta.env.VITE_WS_URL || 'http://localhost:4000';
 
 export const socket = io(URL, {
-  autoConnect: false
+  autoConnect: false,
+  withCredentials: true,
 })
