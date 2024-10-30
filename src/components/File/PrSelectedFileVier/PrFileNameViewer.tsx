4d682c71f096e47de34ef324c721ce1b17f2ee08
pigpgw <ceh20002@naver.com>
2024-10-30 19:09:41 +0900

    refactor:  add PrFilePathViewerProps , PrFileNameViewerProps interface

--- src/components/File/PrSelectedFileVier/PrFileNameViewer.tsx
+++ src/components/File/PrSelectedFileVier/PrFileNameViewer.tsx
@@ -1,4 +1,8 @@
-export const PrFileNameViewer = ({ fileName }: { fileName: string }) => {
+interface PrFileNameViewerProps {
+  fileName: string;
+}
+
+export const PrFileNameViewer = ({ fileName }: PrFileNameViewerProps) => {
   return (
     <span className="item m-1 flex h-7 w-fit items-center border-b-4 border-blue-500 px-2 py-5">
       {fileName}
--- src/components/File/PrSelectedFileVier/PrFilePathViewer.tsx
+++ src/components/File/PrSelectedFileVier/PrFilePathViewer.tsx
@@ -1,4 +1,8 @@
-export const PrFilePathViewer = ({ filePath }: { filePath: string }) => {
+interface PrFilePathViewerProps {
+  filePath: string;
+}
+
+export const PrFilePathViewer = ({ filePath }: PrFilePathViewerProps) => {
   return (
     <span className="item border-b- m-1 flex h-4 w-full items-center p-2">
       {filePath}
