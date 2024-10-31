4d8bc352161b4c09854dae1ff00ace9417d131bd
pigpgw <ceh20002@naver.com>
2024-10-31 13:45:54 +0900

    remove: remove unnecessary code

---
+++ src/components/File/PRBottomFileExplorer.tsx
@@ -0,0 +1,14 @@
+import { prInfoStore } from "@/stores/github.store";
+
+export const PRBottomFileExplorer = () => {
+  const { prInfo } = prInfoStore();
+  return (
+    <div className="p-4">
+      <h3 className="font-medium">Commit Information</h3>
+      <p className="mt-2 text-sm">
+        Branch: {prInfo.requireUserInfo.branchName} →{" "}
+        {prInfo.requestUserInfo.branchName}
+      </p>
+    </div>
+  );
+};
