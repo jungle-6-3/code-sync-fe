1455aaa2458e9d3aed33207d43cbe11cb9a31a2b
loopy-lim <loopy@jnu.ac.kr>
2024-11-04 06:00:32 +0000

    refactor: change changing point

--- src/components/Users/LogoutButton.tsx
+++ src/components/Users/LogoutButton.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@/components/ui/button";
-import { useLogoutMutation } from "@/hooks/Users/useLogoutMutation";
+import { useLogoutMutation } from "@/hooks/Users/useLogOutMutation";
 import { useNavigate } from "react-router-dom";
 
 export const LogoutButton = () => {
-  const logout = useLogoutMutation();
+  const { mutate: logout } = useLogoutMutation();
   const navigate = useNavigate();
 
   const onLogout = async () => {
-    logout.mutate(undefined, {
+    logout(undefined, {
       onSuccess: () => {
         alert("로그아웃 되었습니다.");
         navigate("/");
