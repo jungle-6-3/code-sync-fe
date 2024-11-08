7aa2f414ee3052bbc88d761ce44106d4fb9c79a7
loopy-lim <loopy@jnu.ac.kr>
2024-11-08 19:05:44 +0900

    feat: when go room, reset ydocs

--- src/stores/previousRoom.store.ts
+++ src/stores/previousRoom.store.ts
@@ -4,9 +4,11 @@ interface PreviousRoomStore {
   drawBoard: string,
   setDrawBoard: (drawBoard: string) => void,
   drawBoardYdoc: Y.Doc,
+  setDrawBoardYdoc: (drawBoardYdoc: Y.Doc) => void,
   note: string,
   setNote: (note: string) => void,
   noteYdoc: Y.Doc,
+  setNoteYdoc: (noteYdoc: Y.Doc) => void,
   voice: string,
   setVoice: (voice: string) => void,
 }
@@ -15,9 +17,11 @@ export const usePreviousRoomStore = create<PreviousRoomStore>()((set) => ({
   drawBoard: "",
   setDrawBoard: (drawBoard) => set({ drawBoard }),
   drawBoardYdoc: new Y.Doc(),
+  setDrawBoardYdoc: (drawBoardYdoc) => set({ drawBoardYdoc }),
   note: "",
   setNote: (note) => set({ note }),
   noteYdoc: new Y.Doc(),
+  setNoteYdoc: (noteYdoc) => set({ noteYdoc }),
   voice: "",
   setVoice: (voice) => set({ voice }),
 }));
